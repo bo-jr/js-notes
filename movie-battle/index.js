eef963d54b36165ad22bf753f3a2200a4542d076
Seong Chang <seongchang@Seongs-MacBook-Pro.local>
2021-12-03 16:44:26 -0800

    get stuff checked in

--- movie-battle/index.js
+++ movie-battle/index.js
@@ -1,58 +1,84 @@
-// returns a list of movies
-const fetchData = async (searchTerm) => {
-  // http://www.omdbapi.com/?apikey=[yourkey]&
+let apiKey = '';
+
+createAutoComplete({
+  root: document.querySelector('.autocomplete'),
+  renderOption(movie) {
+    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+    return `
+      <img src="${movie.Poster}" />
+      ${movie.Title} (${movie.Year})
+    `
+  },
+  onOptionSelect(movie) {
+    onMovieSelect(movie)
+  },
+  inputValue(movie) {
+    return movie.Title;
+  },
+  async fetchData(searchTerm) {
+    // http://www.omdbapi.com/?apikey=[yourkey]&
+    const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+        apikey: apiKey,
+        s: searchTerm
+      }
+    });
+  
+    // handling the case where API respones returns "Error"
+    if (response.data.Error) {
+      return [];
+    }
+  
+    return response.data.Search; // this is due to the API reponse
+  }
+});
+
+const onMovieSelect = async movie => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
-      apikey: '',
-      s: searchTerm
-      // i: 'tt0848228'
+      apikey: apiKey,
+      i: movie.imdbID
     }
   });
 
-  // handling the case where API respones returns "Error"
-  if (response.data.Error) { // this is due to the API respose
-    return [];
-  }
-
-  return response.data.Search; // this is due to the API reponse
-}
-
-const root = document.querySelector('.autocomplete'); // div class
-root.innerHTML = `
-  <label><b>Search for a Movie</b></label>
-  <input class="input" />
-  <div class="dropdown">
-    <div class="dropdown-menu">
-      <div class="dropdown-content results"></div>
-    </div>
-  </div>
-`;
-
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-const onInput = async event => {
-  // fetch a list of movies
-  const movies = await fetchData(event.target.value);
-  
-  dropdown.classList.add('is-active'); // it is now <div class="dropdown is-active">
-
-  for (let movie of movies) {
-    // set up the a element
-    const option = document.createElement('a');
-
-    // a content
-    option.classList.add('dropdown-item');
-    option.innerHTML = `
-      <img src="${movie.Poster}" />
-      ${movie.Title}
-    `;
-
-    // actually add the a content on the page
-    resultsWrapper.appendChild(option);
-  }
+  document.querySelector('#summary').innerHTML = movieTemplate(response.data)
 };
 
-// on input (listening for an event), fetch data (has debouncer)
-input.addEventListener('input', debounce(onInput, 500));
+const movieTemplate = (movieDetail) => {
+  return `
+    <article class='media'>
+      <figure class='media-left'>
+        <p class='image'>
+          <img src="${movieDetail.Poster}" />
+        </p>
+      </figure>
+      <div class='media-content'>
+        <div class='content'>
+          <h1>${movieDetail.Title}</h1>
+          <h4>${movieDetail.Genre}</h4>
+          <p>${movieDetail.Plot}</p>
+        </div>
+      </div>
+    </article>
+    <article class='notification is-primary'>
+      <p class='title'>${movieDetail.Awards}</p>
+      <p class='subtitle'>Awards</p>
+    </article>
+    <article class='notification is-primary'>
+      <p class='title'>${movieDetail.BoxOffice}</p>
+      <p class='subtitle'>Box Office</p>
+    </article>
+    <article class='notification is-primary'>
+      <p class='title'>${movieDetail.Metascore}</p>
+      <p class='subtitle'>Metascore</p>
+    </article>
+    <article class='notification is-primary'>
+      <p class='title'>${movieDetail.imdbRating}</p>
+      <p class='subtitle'>IMDB Rating</p>
+    </article>
+    <article class='notification is-primary'>
+      <p class='title'>${movieDetail.imdbVotes}</p>
+      <p class='subtitle'>IMDB Votes</p>
+    </article>
+  `;
+}
