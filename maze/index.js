ac9cabe92ddac348d0e7ba21907dde4e58ea4eba
bo-jr <seongbo5230@gmail.com>
2022-10-06 23:26:58 -0700

    maze generation complete

--- maze/index.js
+++ maze/index.js
@@ -39,32 +39,95 @@ World.add(world, walls)
 // another way to make it is:
 const grid = Array(cells).fill(null).map(() => Array(cells).fill(false)) // row then columns
 
-console.log(grid)
-
 // Grids
 const verticals = Array(cells).fill(null).map(() => Array(cells - 1).fill(false))
 const horizontals = Array(cells - 1).fill(null).map(() => Array(cells).fill(false))
 
 const startRow = Math.floor(Math.random() * cells)
 const startColumn = Math.floor(Math.random() * cells)
-console.log("row and column: " + startRow, startColumn)
+
+// shuffle neighbor array
+const shuffle = (arr) => {
+  let counter = arr.length
+
+  while (counter > 0) {
+    const index = Math.floor(Math.random() * counter)
+    counter--
+    const temp = arr[counter]
+    arr[counter] = arr[index]
+    arr[index] = temp
+  }
+
+  return arr
+}
 
 const stepThroughCell = (row, column) => {
   // If i have visited the cell, then return
+  if (grid[row][column] === true) {
+    return
+  }
 
   // Mark the cell as visited
+  grid[row][column] = true
 
   // Assemble randomly-ordered list of neighbors
+  const neighbors = shuffle([
+    [row - 1, column, 'up'],
+    [row, column + 1, 'right'],
+    [row + 1, column, 'down'],
+    [row, column - 1, 'left'],
+  ])
 
   // For each neighbor...
+  for (let neighbor of neighbors) {
+    const [nextRow, nextColumn, direction] = neighbor
 
-  // See if that neighbor is out of bounds
+    // See if that neighbor is out of bounds - if it is, then just continue loop
+    if (nextRow < 0 || nextColumn >= cells || nextColumn < 0 || nextRow >= cells) {
+      continue
+    }
 
-  // If we have visited that neighbor, continue to next neighbor
+    // If we have visited that neighbor, continue to next neighbor
+    if (grid[nextRow][nextColumn]) {
+      continue
+    }
 
-  // Remove a wall from either horiz/verti
+    // Remove a wall from either horizontal/vertical
+    // vertical
+    if (direction === 'left') {
+      verticals[row][column - 1] = true
+    } else if (direction === 'right') {
+      verticals[row][column] = true
+    }
+    // horizontal
+    if (direction === 'up') {
+      horizontals[row - 1][column] = true
+    } else if (direction === 'down') {
+      horizontals[row][column] = true
+    }
 
-  // Visit that next cell
+    // Visit that next cell
+    stepThroughCell(nextRow, nextColumn)
+  }
 }
 
 stepThroughCell(startRow, startColumn)
+console.log(grid)
+console.log(verticals)
+console.log(horizontals)
+
+// Draw maze walls
+// Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+horizontals.forEach((row) => {
+  row.forEach((open) => {
+    if (open) {
+      return
+    }
+
+    // const wall = Bodies.rectangle(x, y, w, h, { isStatic: true })
+  })
+})
+
+verticals.forEach((column) => {
+  console.log(column)
+})
