7e7c8e42dfc44eddaab3c9335e6a084ee1733447
bo-jr <seongbo5230@gmail.com>
2022-10-06 22:53:57 -0700

    vertices, horizontals, grids set up

--- maze/index.js
+++ maze/index.js
@@ -1,5 +1,6 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
+const cells = 3
 const width = 600
 const height = 600
 
@@ -26,3 +27,44 @@ const walls = [
   Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }),
 ]
 World.add(world, walls)
+
+// Maze Generation
+// const grid = []
+// for (let i = 0; i < 3; i++) {
+//   grid.push([]) // three rows of arrays
+//   for (let j = 0; j < 3; j++) {
+//     grid[i].push(false)
+//   }
+// }
+// another way to make it is:
+const grid = Array(cells).fill(null).map(() => Array(cells).fill(false)) // row then columns
+
+console.log(grid)
+
+// Grids
+const verticals = Array(cells).fill(null).map(() => Array(cells - 1).fill(false))
+const horizontals = Array(cells - 1).fill(null).map(() => Array(cells).fill(false))
+
+const startRow = Math.floor(Math.random() * cells)
+const startColumn = Math.floor(Math.random() * cells)
+console.log("row and column: " + startRow, startColumn)
+
+const stepThroughCell = (row, column) => {
+  // If i have visited the cell, then return
+
+  // Mark the cell as visited
+
+  // Assemble randomly-ordered list of neighbors
+
+  // For each neighbor...
+
+  // See if that neighbor is out of bounds
+
+  // If we have visited that neighbor, continue to next neighbor
+
+  // Remove a wall from either horiz/verti
+
+  // Visit that next cell
+}
+
+stepThroughCell(startRow, startColumn)
